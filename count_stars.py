179675bc84c3df92f9163e16a45d358d929f80ec
Jane <jzheng76@wisc.edu>
2021-09-28 20:42:12 -0500

    count stars

--- count_stars.py
+++ count_stars.py
@@ -3,26 +3,28 @@ import requests # to make HTTP requests
 import argparse # to parse command-line arguments
 import sys
 
-url1="https://api.github.com/users/emaadmanzoor/repos?page=1"
-url2="https://api.github.com/users/emaadmanzoor/repos?page=2"
-url3="https://api.github.com/users/emaadmanzoor/repos?page=3"
-url4="https://api.github.com/users/emaadmanzoor/repos?page=4"
+# Each page only shows 30
 
-r1=requests.get(url1)
-r2=requests.get(url2)
-r3=requests.get(url3)
-r4=requests.get(url4)
+url1 = "https://api.github.com/users/emaadmanzoor/repos?page=1"
+url2 = "https://api.github.com/users/emaadmanzoor/repos?page=2"
+url3 = "https://api.github.com/users/emaadmanzoor/repos?page=3"
+url4 = "https://api.github.com/users/emaadmanzoor/repos?page=4"
+
+r1 = requests.get(url1)
+r2 = requests.get(url2)
+r3 = requests.get(url3)
+r4 = requests.get(url4)
 
 r1=r1.json()
 r2=r2.json()
 r3=r3.json()
 r4=r4.json()
 
-r=r1+r2+r3+r4
+r_total=r1+r2+r3+r4
 
-count=0
+count_stargazers = 0
 
-for i in r:
-  count += i['stargazers_count']
+for i in r_total:
+  count_stargazers += i['stargazers_count']
 
 print(count)
